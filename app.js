845650ecf23f5567984707fe220d8e2013216028
tobihaze <tobihaze_2017@yahoo.co.jp>
2018-08-30 11:16:52 +0000

    メモ化によるアルゴリズムの改善2018

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 'use strict';
+// function fib(n) {
+//     if (n === 0) {
+//         return 0;
+//     } else if (n === 1) {
+//         return 1;
+//     }
+//     return fib(n - 1) + fib(n - 2);
+// }
+// const length = 40;
+// for (let i = 0; i <= length; i++) {
+//     console.log(fib(i));
+// }
+
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
